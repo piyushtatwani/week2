195456420c3679557b36e6f2fcea2ab3b702e731
piyushtatwani <160796793+piyushtatwani@users.noreply.github.com>
2024-03-01 13:44:32 +1300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,57 @@
+# string data type 
+
+# literal assignment
+first = "Piyush"
+last = "Tatwani"
+
+ # print(type(first))
+ # print(type(first) = str)
+ # print(isinstance(first, str))
+
+# constructor function
+# pizza = str("pepperoni")
+# print(type(pizza)
+# print(type(pizza) = str)
+# print(isinstance(pizza, str))
+
+# concatenation
+fullname = first + " " + last
+print(fullname)
+
+fullname += " ! "
+print(fullname)
+
+# casting a number to a string
+decade = str(1980)
+print(type(decade))
+print(decade)
+
+statement = "I like rock music from the " + decade + "s."
+print(statement)
+
+# multiple lines
+multiline = '''
+Hey, how
+
+I was just checking in.
+                              All good?
+
+'''
+print(multiline)                       
+
+# Escaping special characters 
+sentence = 'I\'m back at work!\tHey!\n\nWhere\'s this at\\located?'                                
+print(sentence)
+
+# String Methods
+
+print(first)
+print(first.lower())
+print(first.upper())
+
+
+
+
+
+
+
